8beb20b74545f9c06e3f576f04df3a39a47f8467
6404062610499 <s6404062610499@email.kmutnb.ac.th>
2023-10-03 05:38:55 +0700

    Lab9 DB Connection

---
+++ lab9/w7.php
@@ -0,0 +1,20 @@
+<html>
+<head><meta charset="UTF-8"></head>
+<body>
+
+<form action="w7insert.php" method="post">
+    ชื่อผู้ใช้ : <input type="text" name="username"><br>
+
+    ชื่อสมาชิก : <input type="text" name="name"><br>
+
+    ที่อยู่ : <br>
+    <textarea name="address" rows="3" cols="40"></textarea><br>
+
+    อีเมล : <input type="email" name="email"><br>
+    <input type="submit" value="เพิ่มข้อมูล ">
+    เพิ่มรูปภาพ <label for="img"> :</label>
+    <input type="file" id="img" name="img" accept="image/*"><br>
+    <input type="submit" value="เพิ่มข้อมูล ">
+</form>
+</body>
+</html>
