f736885824dc6ed9c8125b9a1490e27e987c23e5
6404062610499 <s6404062610499@email.kmutnb.ac.th>
2023-10-03 05:22:04 +0700

    Update w6.php

--- lab8/w6.php
+++ lab8/w6.php
@@ -16,7 +16,7 @@ document.location = "w6delete.php?username=" + username;
     while ($row = $stmt->fetch()) {
         echo "ชื่อสมาชิก : " . $row ["name"] . "<br>";
         echo "ที่อยู่ : " . $row ["address"] . "<br>";
-        echo "อีเมล " . $row ["email"] . " บาท <br>";
+        echo "อีเมล " . $row ["email"] . " <br>";
         echo "<a href='editform.php?username=" . $row ["username"] . "'>แก้ไข</a> | ";
         echo "<a href='w6delete.php' onclick='confirmDelete(\"" . $row ["username"] . "\")'>ลบ</a>";
         echo "<hr>\n";
